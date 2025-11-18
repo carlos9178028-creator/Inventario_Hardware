3e3fd79acf12afee181739a78bf930c90d15260a
Carlos <carlos9178028@gmail.com>
2025-11-17 23:18:40 -0500

    Menu de opciones

--- inventario_hardware.py
+++ inventario_hardware.py
@@ -1,1 +1,14 @@
 hardware={}
+while True :
+    print("\nMenú de Inventario de Hardware")
+    print("1. Agregar hardware")
+    print("2. Mostrar inventario")
+    print("3. Salir")
+    try :
+        opcion=int("Elige una opcion del menú:")
+    except ValueError:
+        print("Por favor, ingresa un número válido.")
+        continue
+    else:
+        if opcion>=1 or opcion<=3:
+            
