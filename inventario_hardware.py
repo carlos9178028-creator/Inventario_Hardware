8d8f2c655ab8286b21b0d3d751cf16e0e673e6ad
Carlos <carlos9178028@gmail.com>
2025-11-18 12:44:47 -0500

    opcion 1 mejorada

--- inventario_hardware.py
+++ inventario_hardware.py
@@ -12,14 +12,21 @@ while True :
     else:
         if opcion>=1 and opcion<=3:
             if opcion==1:
-                i=-1
-                for clave,valor in hardware_diccionario.items():
-                    while hardware==clave:
-                        print("El hardware ya existe en el inventario.")
-                        hardware=input("Ingresa el nombre del hardware:")
                 hardware=input("Ingresa el nombre del hardware:")
             cantidad=input("Ingresa la cantidad de hardware:")
-            hardware_diccionario[hardware]=cantidad
-            print(f"Se ha agregado {hardware} con cantidad {cantidad} al inventario.")
-            print(hardware_diccionario)
+            c=""
+            c_n=0
+            for clave,valor in hardware_diccionario.items():
+                if clave==hardware and valor==cantidad:
+                    c=hardware
+                    c_n=cantidad
+                elif clave==hardware and valor!=cantidad:
+                    print(f"La cantidad de {hardware} ha sido actualizada a {cantidad}.")
                 
+            if c=="" and c_n==0:
+               hardware_diccionario[hardware]=cantidad     
+               print(f"Se ha agregado {hardware} con cantidad {cantidad} al inventario.")
+               print(hardware_diccionario)
+            else:
+               print("El hardware ya existe en el inventario con la misma cantidad.")
+                 
