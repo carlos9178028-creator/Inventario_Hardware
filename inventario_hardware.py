f6b5ecb5c6ec9eebbd10a0d20f647d137b0ee6c9
Carlos <carlos9178028@gmail.com>
2025-11-21 22:53:24 -0500

    Version mejorada

--- inventario_hardware.py
+++ inventario_hardware.py
@@ -3,8 +3,8 @@ def menu():
     print("Menú")
     print("-------------")
     print("1. Ingresar hardware")
-    print("2.Eliminar hardware de la lista")
-    print("3.Mostrar inventario")
+    print("2. Mostrar inventario")
+    print("3.Eliminar hardware existente")
     print("4. Agregar cantidad nueva a hardware existente ")
     print("5. Salir")
     try: 
@@ -14,17 +14,35 @@ def menu():
     else:
          if op<1 or op>5:
               print("La opcion no existe")
-         else :  
-             return op
+              return menu()
+         return op 
 def ingresar_hardware(hardware):
      if hardware in HArdware_lista:
           print(f"{HArdware_lista[hardware]} ya existe")
      else:
-          hardware=input("Escribe el hardware que quieres ingresar:")
           cantidad=int(input("Ingresa la cantidad que hay del hardware:"))
           HArdware_lista[hardware]=cantidad
-cap=menu()
+def mostrar():
+     x=0
+     for clave, valor in HArdware_lista.items():
+          print(f"{x+1}.-{clave}:{valor}")
+def eliminar(hardware):
+    if hardware in HArdware_lista:
+            del HArdware_lista[hardware]
+     
 while True:
-     cap
-     if cap==1:
-          h=ingresar_hardware
+    cap=menu()
+    if cap==1:
+          hardware=input("Ingresa el hardware:")
+          h=ingresar_hardware(hardware)
+    elif cap==2:
+         mostrar()
+    elif cap==3:
+        if not HArdware_lista:
+              print("Está vació el inventario")
+        else:
+         hardware_del=input("Escribe el hardware que desea eliminar:")
+         elim=eliminar(hardware_del)
+
+         
+         
