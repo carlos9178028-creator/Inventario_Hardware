0c54afdea9acedf534c8963ab257bc42ece6858c
Carlos <carlos9178028@gmail.com>
2025-11-22 22:50:38 -0500

    Progreso del manejo de archivos

--- inventario_hardware.py
+++ inventario_hardware.py
@@ -8,16 +8,27 @@ def menu():
     print("2. Mostrar inventario")
     print("3.Eliminar Hardware")
     print("4. Salir")
-hardware=input("Ingresa el hardware que deseas agregar:")
-cantidad=int(input("Ingresa la cantidad del hardware:"))
-hardware_diccionario[hardware]=cantidad
-print(hardware_diccionario)
-inventario=pathlib.Path("prueba.txt")
-if inventario.exists():
-    print("Existe")
-    with open(inventario,"w",encoding="utf-8") as f :
-        f.write(f"{hardware_diccionario}")
-else:
-    print("No existe")
+    try:
+     op=int(input("Elige una de las opciones:"))
+    except ValueError:
+       print(f"{op} invalido")
+    else:
+       if op<1 or op>4:
+          print("No se encuentra la opcion")
+          return menu()
+       else:
+          return op
+while True:
+   rpta=menu()
+   if rpta==1:    
+    hardware=input("Ingresa el hardware que deseas agregar:")
+    cantidad=int(input("Ingresa la cantidad del hardware:"))
+    hardware_diccionario[hardware]=cantidad
+    inventario=pathlib.Path("prueba.txt")
+    if inventario.exists():
+      with open(inventario,"w",encoding="utf-8") as f :
+         f.write(f"{hardware_diccionario}")
+   else:
+     print("No existe")
          
          
